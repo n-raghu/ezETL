bfbb6c5bd40b9dbaa1c4df45897816f25069b145
raghu <nraghu366@gmail.com>
2019-05-10 18:11:58 +0530

    saledata final

--- dim/popSalesData.py
+++ dim/popSalesData.py
@@ -1,9 +1,12 @@
 import requests as req
 import json
-from dimlib import pgcnx as dbeng,dtm,pdf,cfg,logError,dataSession,rsq
-from pandas import concat as pConcat,Series as pSeries,merge as pMerge,to_datetime as pDT
+from dimlib import pgcnx as dbeng,dtm,pdf,cfg,logError,rsq
+from pandas import concat as pConcat,Series as pSeries,merge as pMerge
 from collections import OrderedDict as odict,Iterable as cIterable
 
+app='salesforce'
+pgx=dbeng(uri)
+api=odict()
 uri='postgresql://' +cfg['eaedb']['uid']+ ':' +cfg['eaedb']['pwd']+ '@' +cfg['eaedb']['host']+ ':' +str(int(cfg['eaedb']['port']))+ '/' +cfg['eaedb']['db']
 TokenPoint=cfg['salesforce']['token']
 DataPoint=cfg['salesforce']['data']
@@ -11,14 +14,11 @@ tokenParams={'grant_type':'password','client_id':cfg['salesforce']['cid'],'clien
     ,'username':cfg['salesforce']['uid'],'password':cfg['salesforce']['pwd']}
 dataParams={'start_date':'05/01/2019','end_date':'05/02/2019'}
 tokenHeadR={'content-type':'application/x-www-form-urlencoded'}
-
 r=req.post(TokenPoint,params=tokenParams,headers=tokenHeadR)
-
+_record=[{'app':app,'endpoint':r.url,'headers':json.dumps(tokenHeadR),'params':json.dumps(tokenParams),'responsecode':str(r),'responseok':r.ok,'requesttime':dtm.utcnow()}]
+pdf(_record).to_sql('api_traces',index=False,if_exists='append',schema='framework')
 dataHeadR={'accept':cfg['salesforce']['data_ctype'],'content-type':cfg['salesforce']['data_ctype']
         ,'Authorization':'Bearer {}'.format(json.loads(r.text)['access_token'])}
-
-pgx=dbeng(uri)
-api=odict()
 
 def listFlatter(l):
     for el in l:
@@ -38,24 +38,21 @@ for pnt in points:
  idi,hit=pnt
  R=req.post(hit,data=json.dumps(dataParams),headers=dataHeadR)
  api[idi]=pdf(json.loads(R.text))
+ _record=[{'app':app,'endpoint':idi,'headers':json.dumps(dataHeadR),'params':json.dumps(dataParams),'responsecode':str(R),'responseok':R.ok,'requesttime':dtm.utcnow()}]
+ pdf(_record).to_sql('api_traces',index=False,if_exists='append',schema='framework')
  api[idi].columns=map(str.lower,api[idi].columns)
 
-# List all collections
-collectionList=mapper['collection']
-collectionList=list(set(collectionList))
-
 # Create list of tables to be created
-tabFrame=mapper[['point_name','point_col']].copy(deep=True)
-tabFrame['point_col']=tabFrame.groupby('point_name')['point_col','point_name'].transform(lambda x:','.join(x))
+tabFrame=mapper[['point_name','point_col','collection']].copy(deep=True)
+tabFrame['point_col']=tabFrame.groupby('collection')['point_col','point_name'].transform(lambda x:','.join(x))
 tabFrame.drop_duplicates(inplace=True)
-tabList=list(zip(tabFrame['point_name'],tabFrame['point_col']))
+tabList=list(zip(tabFrame['point_name'],tabFrame['point_col'],tabFrame['collection']))
 
 # Create Table Frames
-dataCollectionList=[]
 for tab in tabList:
- idi,col=tab
- col=col.split(',')
- parseCol=[{k.split('.')[0]:k.split('.')[1]} for k in col if "." in k]
+ idi,columnSTR,tabName=tab
+ columnList=columnSTR.split(',')
+ parseCol=[{k.split('.')[0]:k.split('.')[1]} for k in columnList if "." in k]
  if len(parseCol)>0:
   _nu={}
   for _itr in parseCol:
@@ -64,7 +61,7 @@ for tab in tabList:
     key.append(_v)
     _nu[_k]=key
   unpackCols=list(_nu.keys())
-  tabColumns=[_ for _ in col if '.' not in _]
+  tabColumns=[_ for _ in columnList if '.' not in _]
   apiKey=list(mapper['api_keycol'])[0]
   fetchColFromAPI=unpackCols+[apiKey]+tabColumns
   fetchColFromAPI=list(set(fetchColFromAPI))
@@ -86,13 +83,9 @@ for tab in tabList:
    dataTabFrame=fullFrame
   tabColumns+=list(_nu.values())
   tabColumns=list(listFlatter(tabColumns))
-  dataCollectionList.append(dataTabFrame[tabColumns])
+  dataTabFrame.drop_duplicates()
+  dataTabFrame[tabColumns].to_sql(tabName,index=False,if_exists='replace',schema='framework')
  else:
-  dataCollectionList.append(api[idi][col])
+  api[idi][columnList].to_sql(tabName,index=False,if_exists='replace',schema='framework')
 
-# Push Table Frames to datastore
-
-# Purge data objects
-del collectionList
-del mapper
 del api
