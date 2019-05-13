0f1787eaf29e24854ced076561fb3ffecd4a204f
raghu <nraghu366@gmail.com>
2019-05-13 18:16:59 +0530

    nu statements

--- dim/execStatements.py
+++ dim/execStatements.py
@@ -1,5 +1,10 @@
-from dimlib import dataSession,rsq,cfg,pgcnx
-import os
+import sys
+if len(sys.argv)<2:
+	sys.exit('PID not provided... ')
+else:
+	pid=int(sys.argv[1])
+
+from dimlib import dataSession,rsq,cfg,pgcnx,alchemyEXC,logError
 
 uri='postgresql://' +cfg['eaedb']['uid']+ ':' +cfg['eaedb']['pwd']+ '@' +cfg['eaedb']['host']+ ':' +str(int(cfg['eaedb']['port']))+ '/' +cfg['eaedb']['db']
 pgx=pgcnx(uri)
@@ -9,8 +14,12 @@ allStatements=rsq(getStatements,pgx)
 if len(allStatements)>0:
     for idx,row in allStatements.iterrows():
         thisSession=dataSession(uri)
-        thisSession.execute(row['statement'])
-        thisSession.commit()
+        try:
+            _=row['statement']
+            thisSession.execute(_)
+            thisSession.commit()
+        except alchemyEXC.SQLAlchemyError as err:
+            logError(pid,'Addon Scripts',err,uri)
         thisSession.close()
 
 pgx.dispose()
