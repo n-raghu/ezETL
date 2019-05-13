591024bb76aef76bd02f09a821fc2526025310e1
raghu <nraghu366@gmail.com>
2019-05-13 17:05:46 +0530

    statements prototype

--- dim/execStatements.py
+++ dim/execStatements.py
@@ -1,13 +1,16 @@
-from dimlib import dtm,dataSession,rsq,cfg,pgcnx
+from dimlib import dataSession,rsq,cfg,pgcnx
 import os
 
 uri='postgresql://' +cfg['eaedb']['uid']+ ':' +cfg['eaedb']['pwd']+ '@' +cfg['eaedb']['host']+ ':' +str(int(cfg['eaedb']['port']))+ '/' +cfg['eaedb']['db']
 pgx=pgcnx(uri)
-getStatements=''' SELECT statement FROM framework.dim_statements WHERE isactive=true '''
+getStatements=''' SELECT statement FROM framework.statements WHERE isactive=true '''
 allStatements=rsq(getStatements,pgx)
 
-for idx,row in allStatements.iterrows():
-    thisSession=dataSession(uri)
-    thisSession.execute(row['statement'])
-    thisSession.commit()
-    thisSession.close()
+if len(allStatements)>0:
+    for idx,row in allStatements.iterrows():
+        thisSession=dataSession(uri)
+        thisSession.execute(row['statement'])
+        thisSession.commit()
+        thisSession.close()
+
+pgx.dispose()
