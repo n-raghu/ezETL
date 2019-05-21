a061c30f7d43a44562f061d62b996443e1fe0bca
raghu <nraghu366@gmail.com>
2019-05-21 19:20:13 +0530

    yml_file

--- dim/setup_yml_reportdb.py
+++ dim/setup_yml_reportdb.py
@@ -6,8 +6,6 @@ with open('eae.yml','r') as yfl:
 with open('dim.eg.yml','r') as yfl:
     egdim=yaml.safe_load(yfl)
 
-nudim={}
-
 for _key in egdim.keys():
     _val=eae.get(_key,False)
     if not _val:
@@ -15,7 +13,6 @@ for _key in egdim.keys():
         print(warnmsg)
     else:
         egdim[_key].update(_val)
-    nudim[_key]=egdim[_key]
 
 with open('newdim.yml','w') as yfl:
-    yaml.dump(nudim,yfl,default_flow_style=False)
+    yaml.dump(egdim,yfl,default_flow_style=False)
