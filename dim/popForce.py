497d0fb837fe10b16c3cc08091e44b2c9024afb8
raghu <nraghu366@gmail.com>
2019-05-21 16:59:29 +0530

    dynamic salesforce

--- dim/popForce.py
+++ dim/popForce.py
@@ -13,7 +13,7 @@ TokenPoint=cfg['salesforce']['token']
 tokenParams={'grant_type':'password','client_id':cfg['salesforce']['cid'],'client_secret':cfg['salesforce']['secrect']
     ,'username':cfg['salesforce']['uid'],'password':cfg['salesforce']['pwd']}
 _fmt=cfg['salesforce']['dateformat']
-_x=rsq(''' SELECT requesttime FROM framework.api_traces WHERE app='salesforce' ''',pgx)
+_x=rsq(''' SELECT requesttime FROM framework.api_salesforce_tracker WHERE app='salesforce' ''',pgx)
 _sdate=_x['requesttime'].max()
 row_timestamp=dtm.utcnow()
 
@@ -23,20 +23,12 @@ else:
     _sdate=cfg['salesforce']['start_date']
 _fmt='%' +'/%'.join(_fmt.split('/'))
 dataParams={'start_date':_sdate,'end_date':dtm.utcnow().strftime(_fmt)}
-dataParams={'start_date':'01/01/2016','end_date':'05/16/2019'}
 tokenHeadR={'content-type':'application/x-www-form-urlencoded'}
 r=req.post(TokenPoint,params=tokenParams,headers=tokenHeadR)
 _record=[{'app':app,'endpoint':r.url,'headers':json.dumps(tokenHeadR),'params':json.dumps(tokenParams),'responsecode':str(r),'responseok':r.ok,'requesttime':dtm.utcnow()}]
 pdf(_record).to_sql('api_traces',pgx,index=False,if_exists='append',schema='framework')
 dataHeadR={'accept':cfg['salesforce']['data_ctype'],'content-type':cfg['salesforce']['data_ctype']
         ,'Authorization':'Bearer {}'.format(json.loads(r.text)['access_token'])}
-
-def listFlatter(l):
-    for el in l:
-        if isinstance(el, cIterable) and not isinstance(el, (str, bytes)):
-            yield from listFlatter(el)
-        else:
-            yield el
 
 mapper=rsq('SELECT * FROM framework.api_mappers WHERE active=true',pgx)
 tfo=mapper[['point_name','endpoint','collection']].copy(deep=True)
